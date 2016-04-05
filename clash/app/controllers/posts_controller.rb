ed30ddf81a24512a32875a09185c256b16c19f04
phani <phani05353@gmail.com>
2016-04-05 18:19:02 +0000

    4

--- clash/app/controllers/posts_controller.rb
+++ clash/app/controllers/posts_controller.rb
@@ -1,5 +1,44 @@
 class PostsController < ApplicationController
+  before_action :post_find, only: [:edit, :show, :update, :destroy]
   def index
+   @posts = Post.all.order("created_at DESC")
+  end
+  def show
 
+  end
+  def create
+  @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+      else
+      render 'new'
+    end
+  end
+
+  def new
+  @post = Post.new
+  end
+  def edit
+
+  end
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+      else
+      render 'edit'
+    end
+  end
+  def destroy
+  @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def post_find
+  @post = Post.find(params[:id])
+  end
+  def post_params
+  params.require(:post).permit(:title, :link, :description)
   end
 end
